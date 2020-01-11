1f8210b7da02cb73ce061a2f90b408a5cd052ccc
svec-final-year-project-eee <58807307+svec-final-year-project-eee@users.noreply.github.com>
2020-01-11 20:00:13 +0530

    update

--- Simulation/sdata.m
+++ Simulation/sdata.m
@@ -1,7 +1,8 @@
-current = c(:,2:4)
-current(:,4) = 11113
-voltage = v(:,2:4)
-logg = [voltage,current]
-data = xlsread('sdata')
-data = [data;logg]
-xlswrite('sdata',data)
+current = c(:,2:4);
+current(:,4) = 11121;
+current(:,5) = 0;
+voltage = v(:,2:4);
+logg = [voltage,current];
+data = xlsread('sdata');
+data = [data;logg];
+xlswrite('sdata',data);
