fe12877d620e616e857f18ef60f5afc60b4fbc00
svec-final-year-project-eee <58807307+svec-final-year-project-eee@users.noreply.github.com>
2020-01-20 13:45:51 +0530

    pavan
    adding source codes

---
+++ Simulation/sdata1.m
@@ -0,0 +1,8 @@
+current = c(:,2:4);
+current(:,4) = 11110;
+current(:,5) = 0;
+voltage = v(:,2:4);
+logg = [voltage,current];
+data = xlsread('sdata1');
+data = [data;logg];
+xlswrite('sdata1',data);
